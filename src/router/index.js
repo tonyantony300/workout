0ddd8730fd00612157a0486293e7418517b9a054
Tony <tony@caproloktechnologies>
2022-10-30 15:59:48 +0530

    login page and routing I did

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },   
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },   
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },   
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+// Change document titles
+
+// Route guard for auth routes
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div><h1>Home</h1></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  setup() {
+    // Create data / vars
+
+    // Get data
+
+    // Run data function
+
+    return {};
+  },
+};
+</script>
